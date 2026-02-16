ca3aae60e627221827aafcbb53b8963dd3155b52
ACE <jokeacearooni@gmail.com>
2026-02-15 23:39:29 -0600

    Update WebSocket identifyProperties for mobile
    Removed commented code and ensured client identifies as mobile.

--- code.js
+++ code.js
@@ -1,12 +1,4 @@
 const { DefaultWebSocketManagerOptions: { identifyProperties } } = require("@discordjs/ws");
 
-// Set this to trigger the mobile phone icon in the Discord UI
-identifyProperties.browser = "Discord iOS"; 
-// Alternative: identifyProperties.browser = "Discord Android";
-
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
-});
-
-client.login('YOUR_TOKEN');
+// This global override ensures the client identifies as a mobile device
+identifyProperties.browser = "Discord iOS";
